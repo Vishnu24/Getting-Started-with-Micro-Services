bd161ea690851b97698dc8cdf91130691e60f998
Vishnu <vishnu.garg@shephertz.co.in>
2018-02-27 17:46:38 +0530

    Code
    Signed-off-by: Vishnu <vishnu.garg@shephertz.co.in>

--- Tech_Eureka_Server/src/main/java/com/tech/config/TechEurekaServerApplication.java
+++ Tech_Eureka_Server/src/main/java/com/tech/config/TechEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.tech.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class TechEurekaServerApplication {
 
 	public static void main(String[] args) {
