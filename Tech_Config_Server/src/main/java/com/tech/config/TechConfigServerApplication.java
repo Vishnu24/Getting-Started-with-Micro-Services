15e9ed1dfd12d5b2ada2462d57295b79974ed6ee
Vishnu <vishnu.garg@shephertz.co.in>
2018-02-27 09:51:58 +0530

    Config and Eureka Server
    Signed-off-by: Vishnu <vishnu.garg@shephertz.co.in>

---
+++ Tech_Config_Server/src/main/java/com/tech/config/TechConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.tech.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechConfigServerApplication.class, args);
+	}
+}
---
+++ Tech_Eureka_Server/src/main/java/com/tech/config/TechEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.tech.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEurekaServerApplication.class, args);
+	}
+}
